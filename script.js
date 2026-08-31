b503ddaaeaeb812cc1af12f0b8da4e90a6b22935
Vitaly Kiselev <vkiselev@servicechannel.com>
2026-08-31 22:59:51 +0300

    v4

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-async function init(){const d=await fetch("data.json").then(r=>r.json());const app=document.getElementById("app");for(const t of d.tournaments){const el=document.createElement("details");el.innerHTML=`<summary>${t.name} [${t.tier}]</summary><pre>${JSON.stringify(t.results,null,2)}</pre>`;app.appendChild(el)}}init();
+async function init(){
+ const data=await fetch('data.json').then(r=>r.json());
+ const app=document.getElementById('app');
+ for(const t of data.tournaments){
+  const d=document.createElement('details');
+  const n=t.results.nakamura.length;
+  const p=t.results.nepo.length;
+  d.innerHTML=`<summary>${t.name} (${t.tier})</summary>
+  <table>
+  <tr><th>Показатель</th><th>Накамура</th><th>Непомнящий</th></tr>
+  <tr><td>Участий</td><td>${n}</td><td>${p}</td></tr>
+  </table>
+  <pre>${JSON.stringify(t.results,null,2)}</pre>`;
+  app.appendChild(d);
+ }
+}
+init();
